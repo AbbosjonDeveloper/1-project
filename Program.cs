52b3d4ec2c743000816b71b3242324594d1573a5
Abbos Abdisattarov <abdisattarovabbos36@gmail.com>
2022-11-14 23:54:41 +0500

    changed name of name

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 int age = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Enter Kescha's age : ");
 int keschasAge = Convert.ToInt32(Console.ReadLine());
-int comparisonAge = age - keschasAge;
-System.Console.WriteLine($"Age difference between you and Kescha is : {comparisonAge}");
+int ageComparison = age - keschasAge;
+System.Console.WriteLine($"Age difference between you and Kescha is : {ageComparison}");
 
 if (age < keschasAge)
 {
