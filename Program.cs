9fa7e4f46ed209b7a867b647dda50d2b07ef58a6
Abbos Abdisattarov <abdisattarovabbos36@gmail.com>
2022-11-14 15:51:23 +0500

    If function is added!

--- Program.cs
+++ Program.cs
@@ -5,12 +5,19 @@ int keschasAge = Convert.ToInt32(Console.ReadLine());
 int comparisonAge = age - keschasAge;
 System.Console.WriteLine($"Age difference between you and Kescha is : {comparisonAge}");
 
-System.Console.WriteLine($"You are older than  Kescha : {age > keschasAge}");
-System.Console.WriteLine($"Kescha is  older than  you : {age < keschasAge}");
-System.Console.WriteLine($"You are older or equal to Kescha : {age >= keschasAge}");
-System.Console.WriteLine($"Kescha is  older or equal to you : {age <= keschasAge}");
-System.Console.WriteLine($"You and Kescha are equal : {age == keschasAge}");
-System.Console.WriteLine($"You and Kescha are not equal : {age != keschasAge}");
+if (age < keschasAge)
+{
+  System.Console.WriteLine("You are younger than  Kescha!");
+}
+else if(age < keschasAge) 
+{
+  System.Console.WriteLine("You are older than  Kescha!");
+}
+else 
+{
+  System.Console.WriteLine("You are equal!");
+}
+
 
 
 
